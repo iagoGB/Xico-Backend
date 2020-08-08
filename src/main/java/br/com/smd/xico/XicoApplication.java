a0d5c233e35f4432821632a7cd00f31d7ffb0920
Iago Gomes <iagogb@alu.ufc.br>
2020-08-08 09:46:05 -0300

    Commit Inicial

---
+++ src/main/java/br/com/smd/xico/XicoApplication.java
@@ -0,0 +1,13 @@
+package br.com.smd.xico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/smd/xico/XicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.smd.xico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
